ddaab26d1a76e76edb9c0f6c5e34c5466174ca0a
Hossein Yousefi <hossein@identi.app>
2020-07-01 12:49:40 +0200

    second commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,7 +9,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: Column(
+          children: [
+            Spinnable(
+              radius: 200,
+              child: Stack(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    radius: 200,
+                  ),
+                  Positioned(
+                    top: 50,
+                    right: 150,
+                    child: CircleAvatar(backgroundColor: Colors.red, radius: 50),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/spinnable.dart
+++ lib/spinnable.dart
@@ -1,7 +1,62 @@
 library spinnable;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Spinnable extends StatefulWidget {
+  final double radius;
+  final Widget child;
+
+  Spinnable({
+    @required this.radius,
+    @required this.child,
+  })  : assert(child != null),
+        assert(radius != null);
+
+  @override
+  _SpinnableState createState() => _SpinnableState();
+}
+
+class _SpinnableState extends State<Spinnable> with SingleTickerProviderStateMixin {
+  double angle = 0;
+  Offset lockedOn;
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this);
+    _animationController.addListener(() {
+      print('i am listening');
+      setState(() => angle += _animationController.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanStart: (d) => lockedOn = d.localPosition,
+      onPanUpdate: _panUpdateHandler,
+      onPanEnd: _panEndHandler,
+      child: Transform.rotate(angle: angle, child: widget.child),
+    );
+  }
+
+  void _panUpdateHandler(DragUpdateDetails d) {
+    final lockedOnAngle = atan2(lockedOn.dx - widget.radius, lockedOn.dy - widget.radius);
+    final currentAngle = atan2(d.localPosition.dx - widget.radius, d.localPosition.dy - widget.radius);
+    setState(() {
+      lockedOn = d.localPosition;
+      angle += lockedOnAngle - currentAngle;
+    });
+  }
+
+  void _panEndHandler(DragEndDetails d) {}
 }
